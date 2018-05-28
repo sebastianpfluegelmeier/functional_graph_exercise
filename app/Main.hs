23eba39e5a6844338f1dbfd9dab80e60a28e896e
sebastian pfluegelmeier <pfluegelmeier.s@gmx.at>
2018-05-28 17:58:46 +0200

    Add spanning tree flag

--- app/Main.hs
+++ app/Main.hs
@@ -5,30 +5,40 @@ import System.Environment
 import Data.Maybe
 import qualified Data.Set as S
 import Data.Function
+import Data.List (sort)
 
 main :: IO ()
 main = do
         args <- getArgs
-        maybeGraph <- Lib.fromFile (args!!0)
-        if isJust maybeGraph 
+        if length args == 0
             then do
-                let graph = fromJust maybeGraph
-                putStrLn "articulations:"
-                putStrLn $ show $ Lib.articulations graph
-                putStrLn "bridges:"
-                putStrLn $ show $ Lib.bridges graph
-                putStrLn "components:"
-                let componentsSet = Lib.components graph
-                let componentsList = map S.toList componentsSet
-                putStrLn $ show $ componentsList
-                let distance = Lib.distanceMatrix graph
-                putStrLn "distance matrix:"
-                putStrLn $ matrixToAsciiString $ distance
-                putStrLn "eccentricities:"
-                putStrLn $ show $ Lib.eccentricitiesFromDistance distance
-                putStrLn "spanning forest:"
-                putStrLn $ spanningForestToString $ Lib.spanningForestFromComponents graph componentsSet
-            else do putStrLn "Error: invalid input"
+                putStrLn "no arguments passed"
+            else do
+                let twoOrMoreArgs = length args > 1
+                let printForest = if twoOrMoreArgs then args!!1 == "--printForest" else False
+                maybeGraph <- Lib.fromFile (args!!0)
+                if isJust maybeGraph 
+                    then do
+                        let graph = fromJust maybeGraph
+                        putStrLn "articulations:"
+                        putStrLn $ show $ Lib.articulations graph
+                        putStrLn "bridges:"
+                        putStrLn $ show $ sort $ filter (\(x, y) -> x < y) $ Lib.bridges graph
+                        putStrLn "components:"
+                        let componentsSet = Lib.components graph
+                        let componentsList = map S.toList componentsSet
+                        putStrLn $ show $ componentsList
+                        let distance = Lib.distanceMatrix graph
+                        putStrLn "distance matrix:"
+                        putStrLn $ matrixToAsciiString $ distance
+                        putStrLn "eccentricities:"
+                        putStrLn $ show $ Lib.eccentricitiesFromDistance distance
+                        if printForest
+                            then do
+                                putStrLn "spanning forest:"
+                                putStrLn $ spanningForestToString $ Lib.spanningForestFromComponents graph componentsSet
+                            else return ()
+                    else do putStrLn "Error: invalid input"
 
 matrixToAsciiString :: Matrix Int -> String
 matrixToAsciiString matrix = 
